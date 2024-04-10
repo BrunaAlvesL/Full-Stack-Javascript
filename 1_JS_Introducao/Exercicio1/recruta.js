4ac70051333f2b630f65bb1aa8ccbb837c9babbe
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2024-04-10 18:19:25 -0300

    10/04-01

---
+++ 1_JS_Introducao/Exercicio1/recruta.js
@@ -0,0 +1,13 @@
+const nome = prompt("Qual é o seu nome?")
+const sobrenome = prompt("Qual é o seu sobrenome?")
+const estudo = prompt("Qual é o seu campo estudo?")
+const ano = prompt("Qual é o seu ano de nascimento?")
+
+
+
+alert(
+    "Recruta cadastrado com sucesso!\n" +
+    "\nNome completo: " + primeiroNome + " " + sobrenome +
+    "\nCampo de estudo: " + campoDeEstudo +
+    "\nIdade: " + (2022 - anoDeNascimento)
+  )
---
+++ 1_JS_Introducao/aula10/index.js
@@ -0,0 +1,10 @@
+let nome = "Bruna"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+window.alert(nome)
+nome = window.prompt("Insira o seu nome:")
+
+console.log(nome)
