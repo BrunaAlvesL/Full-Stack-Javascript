982b1a4ed23dd947f6ba2ea33dfa93c2ab02bb48
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2024-06-14 19:37:01 -0300

    subi

---
+++ 4_JS_moderno/export_ESM/index.js
@@ -0,0 +1,10 @@
+ //Importação do Export Nomeado e Default
+import { inline } from "./inline.js"
+import defaultInline from "./inline.js"
+import exportDefault, { group, a, b, c, d } from "./non-inline.js"
+
+inline()
+defaultInline()
+
+group()
+exportDefault()
